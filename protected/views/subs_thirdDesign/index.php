4d0a0ad2a49febdc6cff2c3a8b4ad61f9b1a0d2d
Bondartsev Nikita <bondartsev.nikita@gmail.com>
2016-09-02 15:17:08 +0300

    added k50 tracker code

--- protected/views/subs_thirdDesign/index.php
+++ protected/views/subs_thirdDesign/index.php
@@ -673,5 +673,15 @@ foreach(Section::model() -> findAll(array('order' => 'num ASC')) as $section){
         $(".your-phone").mask("+7(999)999-99-99");
     });
 </script>
+<!-- BEGIN K50 TRACKER CODE-->
+<script>
+    (function(c,a,p) {
+        var s = document.createElement(a); s.src = p; s.type = "text/javascript"; s.async =!0; s.readyState ? s.onreadystatechange = function() { if ( s.readyState === "loaded" || s.readyState === "complete" ) { s.onreadystatechange = null; c();}} : s.onload = function () {c();}; var n = document.getElementsByTagName(a)[0]; n.parentNode.insertBefore(s,n); })(function(){
+        k50Tracker.init({
+            siteId: 46841313737734
+        })
+    },"script","https://k50-a.akamaihd.net/k50/k50tracker2.js");
+</script>
+<!-- END K50 TRACKER CODE-->
 </body>
 </html>
