9c22467014b4b11d9cccf9d8e730a79cd83594ad
Julia <yulia_email@mail.ru>
2016-09-20 13:57:09 +0300

    add a word to prices list

--- protected/views/subs_thirdDesign/_single_price.php
+++ protected/views/subs_thirdDesign/_single_price.php
@@ -12,6 +12,6 @@
 ?>
 <tr <?php if ($active) { echo 'class="selected"'; } ?>>
     <td class="price-name"><span><?php echo $price->text; echo $active ? '<img src="'.Yii::app() -> baseUrl.'/img_thirdDesign/superprice.png" alt="superPrice"/>' : '';?></span></td>
-    <td class="price-new"><?php echo $price -> price; ?>p.</td>
+    <td class="price-new">от <?php echo $price -> price; ?>p.</td>
     <td class="price-old"><?php echo $price -> price_old; ?>p.</td>
 </tr>
