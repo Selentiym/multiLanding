26f556d59db61da1d890a431de555f50311d8dfc
Bondartsev Nikita <bondartsev.nikita@gmail.com>
2017-02-10 14:38:02 +0200

    Added spbTomograf site

---
+++ protected/config/spbTomograf.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 10.02.2017
+ * Time: 13:48
+ */
+return SiteDispatcher::mergeArray(
+    require(dirname(__FILE__).'/mrktClinics.php'),
+    [
+        'components' => [
+
+            'dbCustom'=>array(
+                'class' => 'CDbConnection',
+                'connectionString' => 'mysql:host=localhost;dbname=cq97848_landing',
+                'tablePrefix' => 'tomogr_',
+                'emulatePrepare' => true,
+                'username' => 'cq97848_landing',
+                'password' => 'kicker',
+                'charset' => 'utf8',
+            ),
+        ],
+
+
+        'params'=>array(
+            'siteId' => 'spbTomograf',
+            'formLine' => -7
+        ),
+
+    ]
+);
