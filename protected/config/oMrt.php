eba66350ba929eecace91d55b28e0b8145a43f98
Bondartsev Nikita <bondartsev.nikita@gmail.com>
2017-05-04 11:03:56 +0400

    Changed site name

--- protected/config/oMrt.php
+++ protected/config/oMrt.php
@@ -9,7 +9,7 @@ return SiteDispatcher::mergeArray(
     require_once(dirname(__FILE__).'/main.php'),
     require_once(dirname(__FILE__).'/clearRules.php'),
     array(
-        'name' => 'Самая крупная сеть МРТ и КТ диагностических центров',
+        'name' => 'о-мрт.рф',
         'theme' => 'oMrt/oMrtBootstrap',
         'language' => 'oMrt',
         'defaultController' => 'home',
