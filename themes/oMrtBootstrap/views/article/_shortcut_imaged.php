f20e67de1e4babb0fe9501bc4bf7de0d3ff2f852
Bondartsev Nikita <bondartsev.nikita@gmail.com>
2017-06-03 19:09:50 +0400

    Moved link inside a heading

--- themes/oMrtBootstrap/views/article/_shortcut_imaged.php
+++ themes/oMrtBootstrap/views/article/_shortcut_imaged.php
@@ -4,11 +4,11 @@
  */
 ?>
 <div class="col-md-4 col-12 p-1">
-    <a href="<?php echo $this -> createUrl('home/articleView',['verbiage' => $article -> verbiage]); ?>" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div  class="list-group-item list-group-item-action flex-column align-items-start">
         <div class="d-flex w-100 justify-content-between">
-            <h4 class="mb-1 text-center w-100"><?php echo $article -> name; ?></h4>
+            <h4 class="mb-1 text-center w-100"><a  href="<?php echo $this -> createUrl('home/articleView',['verbiage' => $article -> verbiage]); ?>"><?php echo $article -> name; ?></a></h4>
         </div>
         <div class="text-center w-100"><img class="m-1" style="max-height: 300px; max-width:100%;" src="<?php echo ($url = $article -> getImageUrl()) ? $url : Yii::app() -> getTheme() -> baseUrl . '/images/noImgArticle.png'; ?>" alt="<?php echo $article -> name; ?>"/></div>
         <p class="mb-1"><?php echo $article -> description;?></p>
-    </a>
+    </div>
 </div>
