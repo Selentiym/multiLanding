2fdd9b1ccf08e53134adb9bcc031d948cb1f1d2c
Bondartsev Nikita <bondartsev.nikita@gmail.com>
2017-08-15 18:34:25 +0400

    Added favicon

--- themes/mainSpecLib/views/layouts/home.php
+++ themes/mainSpecLib/views/layouts/home.php
@@ -84,7 +84,7 @@ var slider = topSlider('.json_encode($toShowPrices).');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
+    <link href="<?php echo $baseTheme; ?>/images/logo.ico" rel="shortcut icon" type="image/x-icon">
     <?php
     echo Yii::t('scripts','yandexWM');
     $title = $this -> getPageTitle();
